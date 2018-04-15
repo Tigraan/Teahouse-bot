7ed8b545d16a1d78143050862704c5ccbb8a5560
Tigraan <rienenvoyer@gmail.com>
2018-04-15 20:45:05 +0200

    test login

--- scripts/test.py
+++ scripts/test.py
@@ -3,7 +3,11 @@
 """Simple test for cron jobs etc."""
 
 import datetime
+from scripts import login
 import sys
 
 print(datetime.datetime.utcnow(), ' - test executed')
 print('Python PATH is:', sys.path)
+print('I will attempt to login via PWB')
+login.main()
+print('login.main() did not freeze')
